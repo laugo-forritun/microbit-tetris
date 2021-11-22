71d65c5607a90b4a3c822c6d9d22440d2e9e2487
laugo-forritun <77778082+laugo-forritun@users.noreply.github.com>
2021-11-22 15:56:28 +0000

    Add block spawning and movement

--- tetris.js
+++ tetris.js
@@ -4,9 +4,11 @@ let screen_x = 5
 let screen_y = 1
 let SCREEN_WIDTH = 5
 let SCREEN_HEIGHT = 5
+let BLOCK_FALL_SPEED = 1
 let my_points: number[][] = [[1, 2], [2, 3], [3, 4]]
+let block: number[][] = []
 
-function relative_pos(x: number, y: number) {
+function relative_pos(x: number, y: number): number[] {
 	return [x - screen_x, y - screen_y]
 }
 
@@ -17,7 +19,26 @@ function draw() {
 		let rel: number[] = relative_pos(point[0], point[1])
 		led.plot(rel[0], rel[1])
 	}
+	for (let i = 0; i < block.length; i++) {
+		let point: number[] = block[i]
+		let rel: number[] = relative_pos(point[0], point[1])
+		led.plot(rel[0], rel[1])
+	}
 	basic.pause(100)
+}
+
+function spawn_block() {
+	if (block.length == 0) {
+		// No block currently in use
+		console.log("Spawning block")
+		block = [[4, 0], [5, 0], [5, 1], [6, 0]]
+	}
+}
+
+function move_block() {
+	for (let i = 0; i < block.length; i++) {
+		block[i][1] += BLOCK_FALL_SPEED
+	}
 }
 
 function handle_input() {
@@ -30,6 +51,8 @@ function handle_input() {
 }
 
 basic.forever(function () {
-	draw()
 	handle_input()
+	move_block()
+	spawn_block()
+	draw()
 })
